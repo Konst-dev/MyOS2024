58545335546e67c101555e45da89d79d49ad41f5
konst <developerkae@outlook.com>
2024-12-14 23:58:56 +0300

    Добавлена функция чтения сектора диска

--- kernel_module.c
+++ kernel_module.c
@@ -3,16 +3,55 @@ static char str[15] = "Hello World! ";
 static unsigned int cursX = 0, cursY = 0, textAttr = 0x07;
 static char hexStr[17] = "0123456789ABCDEF";
 static char *allMem = (char *)0;
+static unsigned int sysTimer = 0;
 
 static void putChar();
-static void test();
 static void setCursorPosition(int, int);
 static void print(char *c);
-static void test03();
+static void setTextAttribute(int attr);
+static void uintToStr(char *c, unsigned int n);
+static void intToStr(char *c, int n);
+static void byteToHex(char *c, unsigned int n);
+static void wordToHex(char *c, unsigned int n);
+static void dwordToHex(char *c, unsigned int n);
+static unsigned int getMem();
+static void incSysTimer();
+static void delayST(int t);
+static int hdd0Ready();
+static int readSector(unsigned int n, unsigned int bufAddr);
+// static void readSectorTest();
 
 // function 0
-static void osInit()
+static char ramDetectedStr[22] = " Mb of RAM detected\n";
+static char space[3] = ": ";
+char s1[12];
+static void _start()
 {
+  static char *mm = (char *)0x100204;
+  int m = *(int *)mm;
+  int n = 0;
+
+  int mem = getMem();
+  uintToStr(s1, mem);
+  print(s1);
+  print(ramDetectedStr);
+
+  while (n < 100)
+  {
+    uintToStr(s1, n);
+    print(s1);
+    print(space);
+    readSector(n, 0x100000);
+
+    for (int i = 0; i < 513; i++)
+    {
+      videoMem[i * 2] = allMem[0x100000 + i];
+      videoMem[i * 2 + 1] = textAttr;
+    }
+
+    delayST(10);
+    n++;
+  }
 }
 
 // function 1
@@ -200,6 +239,124 @@ static void print(char *c)
   setCursorPosition(cursX, cursY);
 }
 
+// function 10. Дополнительный обработчик прерываний системного таймера
+static void incSysTimer()
+{
+  sysTimer++;
+}
+
+// function 11. задержка на количество прерываний системного таймера
+static void delayST(int t)
+{
+  int curTime = sysTimer + t;
+  while (sysTimer < curTime)
+    ;
+}
+
+// function 12. Проверка готовности HDD0;
+static int hdd0Ready()
+{
+  int ready = 0;
+  __asm__("pusha\n\t"
+          "movl $4, %%ebx\n\t"
+          "movl $0xE0, %%eax\n\t"
+          "movl $0x1F6, %%edx\n\t"
+          "out %%al, %%dx\n\t"
+          "movl $0x1F7, %%edx\n\t"
+          "_busy:\n\t"
+          "xor %%ecx, %%ecx\n\t"
+          "movl $0xFFFF, %%ecx\n\t"
+          "_lp1:\n\t"
+          "loop _lp1\n\t"
+          "dec %%bx\n\t"
+          "jz _stp\n\t"
+          "in %%dx,%%al\n\t"
+          "test $0b10001000,%%al\n\t"
+          "jnz _busy\n\t"
+          "test $0b01000000,%%al\n\t"
+          "jz _busy\n\t"
+          "movl $1, %0\n\t"
+
+          "_stp:\n\t"
+          "popa\n\t"
+          : "=r"(ready)
+          :
+          : "cc");
+  return ready;
+}
+
+// function 13. Читает сектор с диска. n-номер сектора, bufAddr-адрес буфера
+static int readSector(unsigned int n, unsigned int bufAddr)
+{
+  int done = 0;
+  __asm__("pusha\n\t"
+          "movl $0x3F6, %edx\n\t"
+          "movl $2, %eax\n\t"
+          "out %al, %dx\n\t");
+
+  if (hdd0Ready())
+  {
+    __asm__("movl %[arg_a], %%ecx\n\t"
+            "push %%ecx\n\t"
+            "shr $24, %%ecx\n\t"
+            "or $0xE0, %%cl\n\t"
+            "movl $0x1F6, %%edx\n\t"
+            "mov %%cl, %%al\n\t"
+            "out %%al, %%dx\n\t"
+
+            "pop %%eax\n\t"
+            "movl $0x1F3, %%edx\n\t"
+            "out %%al, %%dx\n\t"
+            "shr $8, %%eax\n\t"
+            "movl $0x1F4, %%edx\n\t"
+            "out %%al, %%dx\n\t"
+            "shr $8, %%eax\n\t"
+            "movl $0x1F5, %%edx\n\t"
+            "out %%al, %%dx\n\t"
+
+            "movl $1, %%eax\n\t"
+            "movl $0x1F2, %%edx\n\t"
+            "out %%al, %%dx\n\t"
+            "mov $0x20, %%al\n\t"
+            "movl $0x1F7, %%edx\n\t"
+            "out %%al, %%dx\n\t"
+
+            "movl $0x3F6, %%edx\n\t"
+            "_l001:\n\t"
+            "in %%dx,%%al\n\t"
+            "test $0x80,%%al\n\t"
+            "jnz _l001\n\t"
+
+            "movl $0x1F7, %%edx\n\t"
+            "_l002:\n\t"
+            "in %%dx,%%al\n\t"
+            "test $0x08,%%al\n\t"
+            "jz _l002\n\t"
+
+            "push %%es\n\t"
+            "movl $0x10, %%eax\n\t"
+            "movw %%ax, %%es\n\t"
+            "movl %[arg_b], %%edi\n\t"
+            "cld\n\t"
+            "movl $256, %%ecx\n\t"
+            "movl $0x1F0, %%edx\n\t"
+            "rep insw\n\t"
+
+            "movl $0x3F6, %%edx\n\t"
+            "movl $0, %%eax\n\t"
+            "out %%al, %%dx\n\t"
+
+            "pop %%es\n\t"
+            "popa\n\t"
+            :
+            : [arg_a] "m"(n), [arg_b] "m"(bufAddr)
+            : "cc");
+    done = 1;
+  }
+  return done;
+}
+
+/*
 static void test03()
 {
   for (int i = 0; i < 1000; i++)
@@ -241,3 +398,4 @@ static void putChar(const char *s)
     videoMem[i * 2 + 1] = 0x0F;
   }
 }
+*/
--- sys.sh
+++ sys.sh
@@ -8,6 +8,7 @@ ld -m elf_i386 -Ttext $fsz -e _start  $fn.o -o $fn.img
 objcopy -O binary -R .comment -S $fn.img $fn.bin
 sudo cat ./kernel24.temp $fn.bin > kernel24
 cp -rf ./kernel24 /home/konst/share/Projects/NASM/v2024
+ndisasm -b 32 $fn.bin > $fn.lst
 rm -f ./kernel24.temp
 rm -f ./$fn.txt
 rm -f ./$fn.ftb
